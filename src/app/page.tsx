25816bcb029d05abe90e513c6b52e7c8043fdded
mnz02 <abdulminhaz47@gmail.com>
2024-09-30 02:33:25 +0530

    Refactor code to add HeroSection component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/Hero";
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   );
 }

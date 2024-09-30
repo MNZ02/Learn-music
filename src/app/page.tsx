91c360c21bf6a27a02658e0ad599b2bcea16b6eb
mnz02 <abdulminhaz47@gmail.com>
2024-10-01 01:56:28 +0530

    Refactor code to add HeroSection, FeaturedCourses, and WhyChooseUs components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
+import React from "react";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/Hero";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </>
   );
 }
